c1849f08546455782471cd609d00d16d5b535521
Jose Casado <jcasado6@yahoo.com>
2019-12-03 21:50:01 -0500

    restructured

--- gameofwar.js
+++ gameofwar.js
@@ -1,66 +1,127 @@
-class Deck {
-  constructor() {
-    this.deck = [];
-
-    let suits = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
-    let values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
-
-    for (let suit in suits) {
-      for (let value in values) {
-        this.deck.push(`${values[value]} of ${suits[suit]}`);
-      }
-    }
-  }
-
-  shuffle(){
-    const deck = this.deck;
-    let m = deck.length, i;
-    while(m){
-      i = Math.floor(Math.random() * m--);
-      [deck[m], deck[i]] = [deck[i], deck[m]];
-    }
+class Card {
+  constructor (rank, suit, value) {
+    this.rank = rank
+    this.suit = suit
+    this.value = value
   }
 }
 
+class Deck {
+  constructor() {
+    this.cards = []
+    
+    let ranks = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "Jack",
+      "Queen",
+      "King",
+      "Ace"
+    ]
+    
+    let suits = [
+      "Heart",
+      "Club",
+      "Diamond",
+      "Spade"
+    ]
+  
+    /** Imperative: with `for` loops */
+    for (let i = 0; i < ranks.length; i++) {
+      for (let j = 0; j < suits.length; j++) {
+        let rank = ranks[i]
+        let suit = suits[j]
+        let value = i
+        
+        this.cards.push(new Card(rank, suit, value))
+      }   
+    }
+    
+    /** Declarative: with `Array.forEach` methods */
+  //   ranks.forEach((rank, value) => {
+  //     suits.forEach(suit => this.cards.push(new Card(rank, suit, value)))
+  //   })
+  } // end of Deck constructor
+}
+  const deck = new Deck()
+  /** for shuffling, research how to shuffl the contnents of an Array.
+   * if you see "Fisher-Yates" or "Knuth", your getting warm!
+   */
 
-const deck1 = new Deck();
-deck1.shuffle()
-console.log(deck1.deck);
+//shuffle deck of cards 
+  function shuffle(array) {
+  
+      var currentIndex = array.length, temporaryValue, randomIndex;
+    
+      while (currentIndex > 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -- ;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+    
+      return array;
+}
 
+newDeck = deck.cards;
+shuffle(newDeck)
+console.log(newDeck)
+// const deck = new Deck()
+// shuffle(deck)
+// console.log(deck.cards)
 
-array = deck1.deck
-player1 = array.splice(26,52);
-player2 = array.splice(0,26);
+player1 = newDeck.splice(26,52);
+player2 = newDeck.splice(0,26);
 console.log(player1)
 console.log(player2)
 
-
-// start the game and compare cards
-  function compare() {
-      let card1 = player1.shift()
-        console.log(card1)
-      let card2 = player2.shift()
-        console.log(card2)
-    if (card1.values > card2.values) {
-        console.log("Player 1 wins!");
-        player1.push(card1, card2);
-        console.log(player1);
-        console.log(player2);
-    } else if (card1.values < card2.values) {
-        console.log("Player 2 wins!");
-        player2.push(card1, card2);
-        console.log(player1);
-        console.log(player2);
-    } else {
-        console.log ("Its a tie")
-    };
+function didSomeoneWin(){
+  if (player1.length == 52){
+      console.log("Player 1 Wins!")
+  } else if (player2.length == 52) {
+      console.log("Player 2 Wins!")
+  } else{
+      console.log('check')
+  }
 }
-compare()
+
+function compare() {
+  let card1 = player1.shift()
+    console.log(card1)
+  let card2 = player2.shift()
+    console.log(card2)
+if (card1.value > card2.value) {
+    console.log("Player 1 wins!");
+    player1.push(card1, card2);
+    console.log(player1);
+    console.log(player2);
+} else if (card1.value < card2.value) {
+    console.log("Player 2 wins!");
+    player2.push(card1, card2);
+    console.log(player1);
+    console.log(player2);
+} else {
+    console.log ("Its a tie")
+}; 
+didSomeoneWin()
+}
+
+// --->run "compare()" in the console to play the game
+
+
+
+
+
 
 // go to war function 
-
-// function war ()
-
 
 
 
